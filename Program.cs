cc10d511f5ab99e78bdd36fe25222c8735171fdd
RoL4ik <107209765+RoL4ik@users.noreply.github.com>
2023-10-25 12:56:40 +0300

    The code files

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace project {
+    
+    class Program {
+        
+        static void Main() {
+
+        byte number = 10;
+        byte number2 = 20;
+
+
+        if (number2 > number) {
+            Console.Write("самое большое число, ");
+            Console.Write(number2);
+            Console.ReadKey();
+        }
+
+        else if(number == number2) {
+
+            Console.WriteLine("Все чилса равны");
+            Console.ReadKey();
+
+        }
+
+
+        else {
+            Console.WriteLine("число не сходиться");
+            Console.ReadKey();
+        }
+
+
+        }
+
+    }
+
+}
